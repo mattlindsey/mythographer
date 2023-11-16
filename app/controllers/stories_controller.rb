b8c4c4329841b7c195121cdb7b602252df8c6d33
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-11-16 17:39:36 -0500

    Create story in background job

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,23 +15,11 @@ class StoriesController < ApplicationController
   end
 
   def create
-    llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
-
-    # TODO: Get name from db instead of hardcoding
-    if params[:story][:mythology_id].eql?('1') then m = 'Greek'
-    elsif params[:story][:mythology_id].eql?('2') then m = 'Roman'
-    elsif params[:story][:mythology_id].eql?('3') then m = 'Norse'
-    end
-
     @story = Story.new(story_params)
-    @story.body = "Generating..."
+    @story.body = "Generating... Refresh soon!"
 
     if @story.save
-      prompt = Langchain::Prompt::PromptTemplate.new(template: "Tell me a very short story from {mythology} mythology that would be appropriately titled {title}.", input_variables: ["mythology", "title"])
-      prompt_text = prompt.format(mythology: m, title: @story.title)
-      @story.body = llm.complete(prompt: prompt_text).completion
-      @story.save
-
+      StoryCreateJob.perform_later(@story.id)
       redirect_to @story
     else
       render 'new', status: :unprocessable_entity
---
+++ app/jobs/story_create_job.rb
@@ -0,0 +1,16 @@
+class StoryCreateJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"])
+
+    story = Story.find(args[0])
+    mythology_name = Mythology.find(story.mythology_id).name
+
+    prompt = Langchain::Prompt::PromptTemplate.new(template: "Tell me a very short story from {mythology_name} mythology that would be appropriately titled {title}.", input_variables: ["mythology_name", "title"])
+    prompt_text = prompt.format(mythology_name: mythology_name, title: story.title)
+    story.body = llm.complete(prompt: prompt_text).completion
+    
+    story.save
+  end
+end
