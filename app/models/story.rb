0b02fa7756fc72ecf8a5920ef30d297eae1135aa
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-12-02 16:02:58 -0500

    Add story tests

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,16 +2,16 @@ class Story < ApplicationRecord
   belongs_to :mythology
   validates :title, presence: true
   validates :body, presence: true
-  validates :creativity, presence: true, inclusion: {in: %w[Conservative Balanced Inventive]}
 
   CREATIVITY_TEMPS = {
-    Conservative: 0.2,
-    Balanced: 0.5,
-    Inventive: 0.8
+    "Conservative" => 0.2,
+    "Balanced" => 0.5,
+    "Inventive" => 0.8
   }.freeze
+  validates :creativity, presence: true, inclusion: {in: CREATIVITY_TEMPS.keys}
 
   def creativity_temp
-    CREATIVITY_TEMPS[creativity.to_sym]
+    CREATIVITY_TEMPS[creativity]
   end
 
   after_update -> { broadcast_replace_to "stories" }
