db280521322d07f652dd00cb85ff564e9b33e422
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-12-02 14:05:09 -0500

    Pass tempearature to llm for creativity

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,5 +4,15 @@ class Story < ApplicationRecord
   validates :body, presence: true
   validates :creativity, presence: true, inclusion: {in: %w[Conservative Balanced Inventive]}
 
+  CREATIVITY_TEMPS = {
+    Conservative: 0.2,
+    Balanced: 0.5,
+    Inventive: 0.8
+  }.freeze
+
+  def creativity_temp
+    CREATIVITY_TEMPS[creativity.to_sym]
+  end
+
   after_update -> { broadcast_replace_to "stories" }
 end
