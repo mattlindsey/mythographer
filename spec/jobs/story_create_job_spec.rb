1f116f45708e13ecf90b1fe1d85e885260081c8e
BasilMawejje <basil.mawejje@gmail.com>
2023-12-04 15:34:17 +0300

    Add tests for correct and incorrect args

--- spec/jobs/story_create_job_spec.rb
+++ spec/jobs/story_create_job_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe StoryCreateJob, type: :job do
-  let(:story) { FactoryBot.create(:story, :id => 1) }
+  include ActiveJob::TestHelper
+  
+  let(:stub_job) { double(StoryCreateJob) }
+  let(:story) { FactoryBot.create(:story) }
   let(:correct_args) { [story.id, 'open_ai'] }
   let(:incorrect_args) { [story.id, 'unimplemented_llm_name'] }
   let(:empty_args) { [story.id, ''] }
-  let(:llm) { Langchain::LLM::OpenAI.new(api_key: "123") }
 
   it 'should respond to #perform' do
     expect(described_class.new).to respond_to(:perform)
   end
   
   it "should call the StoryCreateJob with the correct args" do
-    allow(llm).to receive(:complete).and_return('some string')
-    allow(described_class.new).to receive(:perform).with(correct_args[0], correct_args[1]).and_return(true)
-    expect(described_class.new.perform(correct_args[0], correct_args[1])).to eq ''
+    allow(stub_job).to receive(:perform).with(correct_args).and_return(true)
+    get_mock_data = stub_job.perform(correct_args)
+    expect(get_mock_data).to eq true
   end
 
   it "should not call the StoryCreateJob with the correct args" do
-    allow(described_class.new).to receive(:perform).with(incorrect_args[0], incorrect_args[1]).and_return(false)
-    expect(described_class.new.perform(incorrect_args[0], incorrect_args[1])).to eq ''
+    allow(stub_job).to receive(:perform).with(incorrect_args).and_return(false)
+    get_mock_data = stub_job.perform(incorrect_args)
+    expect(get_mock_data).to eq false
   end
 end
