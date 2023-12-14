693e9ac2ceb36bf7ec901b1f9239917efe3607d9
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-12-14 15:23:19 -0500

    Fix model assoc

--- app/models/god.rb
+++ app/models/god.rb
@@ -1,5 +1,5 @@
 class God < ApplicationRecord
-  belongs_to :mythology, dependent: :restrict_with_error
+  belongs_to :mythology # , dependent: :restrict_with_error <- gives error on db:setup
   belongs_to :pantheon
 
   has_many :storygods, dependent: :destroy
