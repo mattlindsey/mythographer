b7f548687254cfe3fb29fc2e9b0418c51752e24c
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-01-20 13:11:59 -0500

    Fix nested forms

--- app/jobs/story_create_job.rb
+++ app/jobs/story_create_job.rb
@@ -3,30 +3,26 @@ class StoryCreateJob < ApplicationJob
 
   def perform(*args)
     story = Story.find(args[0])
-    mythology_name = story.mythology.name
-    instructions = story.instructions
-    llm_name = story.llm_name
 
-    case llm_name
+    case story.llm_name
     when "openai"
       llm = Langchain::LLM::OpenAI.new(api_key: ENV["OPENAI_API_KEY"], llm_options: {temperature: story.creativity_temp})
     when "google"
       llm = Langchain::LLM::GooglePalm.new(api_key: ENV["GOOGLE_PALM_API_KEY"], default_options: {temperature: story.creativity_temp})
     else
-      logger.error "Unknown LLM: #{llm_name}"
+      logger.error "Unknown LLM: #{story.llm_name}"
     end
 
     story_template = Langchain::Prompt.load_from_path(file_path: "app/prompts/story_create_template.yaml")
-    prompt_text = story_template.format(mythology_name: mythology_name, title: story.title)
+    prompt_text = story_template.format(mythology_name: story.mythology.name, title: story.title)
+    prompt_text ||= "TODO: Fix this" # TODO: story_create_job_spec.rb fails without this line
 
     role_template = Langchain::Prompt.load_from_path(file_path: "app/prompts/story_role_template.yaml")
     StoryGod.where(story_id: args[0]).find_each do |g|
-      god_name = g.god.name
-      role = g.role
-      prompt_text += " " + role_template.format(god: god_name, role: role)
+      prompt_text += " " + role_template.format(god: g.god.name, role: g.role)
     end
 
-    prompt_text ||= "TODO: Fix this" # TODO: story_create_job_spec.rb fails without this line
+    instructions = story.instructions
     if instructions
       prompt = prompt_text + " Additional instructions for generating the story are " + instructions
     end
