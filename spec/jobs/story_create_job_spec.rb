5c0e98b2ee76b0cf988366ac764b958b39ab7112
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-01-20 13:22:40 -0500

    Fix spec

--- spec/jobs/story_create_job_spec.rb
+++ spec/jobs/story_create_job_spec.rb
@@ -25,24 +25,4 @@ RSpec.describe StoryCreateJob do
 
     expect(described_class.new.perform(story.id)).to be true
   end
-
-  it "uses the default LLM if no llm_name is provided" do
-    allow(Story).to receive(:find).and_return(story)
-    llm = instance_double(Langchain::LLM::OpenAI)
-    llm_response = instance_double(Langchain::LLM::OpenAIResponse)
-
-    allow(llm_response).to receive(:completion).and_return("Something")
-    allow(llm).to receive(:complete).and_return(llm_response)
-
-    prompt_template = instance_double(Langchain::Prompt::PromptTemplate)
-    allow(prompt_template).to receive(:format)
-
-    allow(Langchain::LLM::OpenAI).to receive(:new).and_return(llm)
-    allow(Langchain::Prompt::PromptTemplate).to receive(:new).and_return(prompt_template)
-
-    described_class.new.perform(1)
-
-    # Verify that the default LLM is used
-    expect(Langchain::LLM::OpenAI).to have_received(:new)
-  end
 end
