28ca6140fa7bd0745327be1e59d357fb6e8ebea6
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-01-20 09:14:45 -0500

    Cleanup (#43)
    * Cleanup tests
    
    * Rubocop fixes

--- spec/jobs/story_create_job_spec.rb
+++ spec/jobs/story_create_job_spec.rb
@@ -23,25 +23,8 @@ RSpec.describe StoryCreateJob do
     allow(Langchain::LLM::OpenAI).to receive(:new).and_return(llm)
     allow(Langchain::Prompt::PromptTemplate).to receive(:new).and_return(prompt_template)
 
-    expect(described_class.new.perform(story.id, "openai")).to be true
+    expect(described_class.new.perform(story.id)).to be true
   end
-
-  # it "calls the StoryCreateJob with the correct args for google" do
-  #   allow(Story).to receive(:find).and_return(story)
-  #   llm = instance_double(Langchain::LLM::GooglePalm)
-  #   llm_response = instance_double(Langchain::LLM::GooglePalmResponse)
-
-  #   allow(llm_response).to receive(:completion).and_return("Something")
-  #   allow(llm).to receive(:complete).and_return(llm_response)
-
-  #   prompt_template = instance_double(Langchain::Prompt::PromptTemplate)
-  #   allow(prompt_template).to receive(:format)
-
-  #   allow(Langchain::LLM::GooglePalm).to receive(:new).and_return(llm)
-  #   allow(Langchain::Prompt::PromptTemplate).to receive(:new).and_return(prompt_template)
-
-  #   expect(described_class.new.perform(story.id, "google")).to be true
-  # end
 
   it "uses the default LLM if no llm_name is provided" do
     allow(Story).to receive(:find).and_return(story)
@@ -57,7 +40,7 @@ RSpec.describe StoryCreateJob do
     allow(Langchain::LLM::OpenAI).to receive(:new).and_return(llm)
     allow(Langchain::Prompt::PromptTemplate).to receive(:new).and_return(prompt_template)
 
-    described_class.new.perform(1) # No llm_name provided
+    described_class.new.perform(1)
 
     # Verify that the default LLM is used
     expect(Langchain::LLM::OpenAI).to have_received(:new)
--- spec/system/story_tests_spec.rb
+++ spec/system/story_tests_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "StorySystemTests" do
-  it "gives error message on empty title and url", skip: "TODO" do
+  it "gives error message on empty title and url" do
     visit new_story_path
     click_link_or_button "Generate Story"
 
     expect(page).to have_content("Title can't be blank")
-    # expect(Story.count).to eq(0)
+    # expect(Story.count).to eq(1) # only seeded story
   end
 
-  it "creates new story", skip: "TODO" do
+  it "creates new story" do
     visit stories_path
     assert_selector "h1", text: "Story List"
 
@@ -17,9 +17,9 @@ RSpec.describe "StorySystemTests" do
     assert_selector "h1", text: "New Story"
 
     fill_in "Title", with: "Story about Zeus"
-    # select "Greek", from: "story_mythology_id"
-    fill_in "god_id", with: 1
-    # fill_in "llm_name", with: "openai"
+    select "Greek", from: "story_mythology_id"
+    select "openai", from: "story_llm_name"
+    select "Zeus", from: "story_storygods_attributes_0_god_id"
     click_link_or_button "Generate Story"
 
     assert_selector "h1", text: "Story about Zeus"
